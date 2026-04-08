51e681cc88cc68cb0b22eed60c0f1533eef4d5d8
asinggit <contactarpitasingh89@gmail.com>
2026-04-08 19:39:11 +0530

    Create csvloading2.py

---
+++ csvloading2.py
@@ -0,0 +1,11 @@
+
+val table: String = config.getString("sink.db.tbl")
+val files: String = config.getString("source.file")
+df= spark.read.csv(
+    files, 
+    header=True, 
+    mode="DROPMALFORMED", 
+    schema=schema
+)
+
+df.write.saveAsTable(table)
